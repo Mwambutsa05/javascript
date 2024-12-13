7b1eb604f4455fbb6dc6f090e3e65a7f6c6551a8
mwambutsa05 <mwambutsadaryce@gmail.com>
2024-12-13 10:19:16 +0200

    java assignements & challenges

--- Day1/variables.js
+++ Day1/variables.js
@@ -1,16 +1,2 @@
-function myFunction(a, b){
-return (a+b);
-console .log ( myFunction(1, 2) );
-console .log ( myFunction(1, 10));
-console .log ( myFunction(99, 1)) ;
-}
-function myFunction(a, b){
-return a===b
-console .log ( myFunction(3, 3) )
-console .log ( myFunction('10', '10') )
-}
-function
-myFunction(a){
-return ( typeof a )
-console .log ( myFunction(1)) 
-}
+
+
---
+++ Day3/challenge2.js
@@ -0,0 +1,13 @@
+let age= 18;
+if (age<13){
+    console. log ( "You are a child.")
+}
+else if (age>13 || age<=19) {
+    console. log ("You are a teenager.")
+}
+else if (age<20){
+    console.log ("You are a young adult.")
+}
+else{
+    console.log ("You are an adult.")
+}
