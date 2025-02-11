f09a385f8f42511bb88c780b552d27d7b57dd207
mwambutsa05 <mwambutsadaryce@gmail.com>
2025-02-11 13:29:36 +0200

    Revising javascript from scratch

---
+++ script.js
@@ -0,0 +1,77 @@
+// alert("Mwmabutsa Daryce");
+// console.log("Trying console log");
+// console.warn("this is a warning message");
+// console.table({name: "mwamnutsa" , age:"20"});
+// let counter = 10;
+// counter++;
+// console.log(counter);
+// let firstFavNumber = 10;
+// secondFavNumber = 10;
+// console.log(firstFavNumber+secondFavNumber);
+// console.log(firstFavNumber*secondFavNumber);
+// console.log(firstFavNumber/secondFavNumber);
+// console.log(firstFavNumber**secondFavNumber);
+// console.log(firstFavNumber-secondFavNumber);
+// let isJsProgramingLanguage = true;
+// let isJsHard = false;
+// let favNUm = 20;
+// console.log(favNUm + undefined);
+// console.log(isJsProgramingLanguage);
+// console.log(isJsHard);
+// let firstName = "    Mwambutsa";
+// let lastName = "Daryce";
+// let fullName = firstName.concat(lastName);
+// console.log(fullName);
+// lastName += "Ineza";   //this is called append is adding Ineza on the last name
+// console.log(lastName);
+// console.log(fullName.toLowerCase());
+// console.log(fullName.toUpperCase());
+// console.log(lastName.slice(0 , 4));  //first num in () start from zero index but second num we count as usual index
+// console.log(lastName.split(" ").join("-"));
+// console.log(lastName.includes("D"));    //this looks if the D includes in the lastName variable
+// console.log(firstName.trim());   //trimm removes all the blanc space
+// let fName = `${firstName} ${lastName} Ineza`; //the dollar sign is for adding a text in another text
+// console.log(fName);
+
+// let favActorFname = "Mwambutsa";
+// let favActorLname = "Daryce";
+// let fulName = favActorFname.concat(favActorLname);
+// let upperCase = fulName.toUpperCase();
+// let message = `my fave actor is ${upperCase} and she is cute ${"her best show is silcon valley"}`;
+// console.log(message);
+
+// Type conversation==================================================================
+// let money = "50";
+// money = parseInt(money);    // this change string into an integer 
+// money = money.toString(); // this change an integer into a string
+// console.log(typeof money);
+
+
+// let score = 90;
+// let grade;
+// switch(score){
+//     case 90:
+//         grade = "A"
+//         console.log(grade);
+//         break;
+//     case 80:
+//       grade = "B";
+//       console.log(grade);
+//       break;
+//     default:
+//         grade ="F"; 
+//         console.log(grade);  
+// }
+
+
+
+for (let i=0; i<=1000; i++){
+    console.log("Mwambutsa", i);
+    
+}
+
+
+
+
+
+
